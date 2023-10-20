5e288a0b1016759f9235b595d597c0467b9d2207
bajjirulz <bajjirulz@gmail.com>
2023-10-20 15:10:53 +0530

    just added nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,31 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import { Routes, Route } from 'react-router-dom'
+
 import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './pages/Home'
+import About from './pages/About'
+import Services from './pages/Services'
+import Contact from './pages/Contact'
+
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <Header></Header>
+    
+      <Header/>  
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path="about" element={<About/>}/>
+        <Route path="services" element={<Services/>}/>
+        <Route path="contact" element={<Contact/>} />
+        <Route path="*" element={<Home />} />
+
+      </Routes>
+      <Footer/>  
     </>
   );
 }
